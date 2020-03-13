1e3c81fc16efd7e58fe0ffa44cf3a3551ac22009
Peng_shixin <shixinpeng0429@163.com>
2020-03-13 23:55:17 +0800

    变量常量操作

--- kotlin/src/com/kotlin/lecture01/HelloKotlin.kt
+++ kotlin/src/com/kotlin/lecture01/HelloKotlin.kt
@@ -1,7 +1,20 @@
 package com.kotlin.lecture01
 
 fun main(args: Array<String>) {
-   println(sum(3,1))
+    println(sum(3, 1))
+    sumPrint(5, 3)
+    printTemplate(1, 1)
 }
 
-fun sum(a: Int,b: Int): Int = a+b
+fun sum(a: Int, b: Int): Int = a + b
+
+fun sumPrint(a: Int, b: Int): Unit {
+    println(a + b)
+}
+
+/**
+ * 字符串模板a
+ */
+fun printTemplate(a: Int, b: Int) {
+    println("$a + $b = ${a + b}")
+}
---
+++ kotlin/src/com/kotlin/lecture02/HelloKotlin2.kt
@@ -0,0 +1,12 @@
+package com.kotlin.lecture02
+
+fun main() {
+    val a: Int = 1
+    var b = 2
+    b = 1
+
+    var x = 10;
+    var y: Byte = 20
+
+    x = y.toInt()
+}
