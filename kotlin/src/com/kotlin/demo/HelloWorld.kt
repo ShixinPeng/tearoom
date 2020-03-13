46ac718cb262e0e64c64e63369a662d260001245
Peng_shixin <shixinpeng0429@163.com>
2020-03-13 23:14:23 +0800

    初认识kotlin

--- kotlin/src/com/kotlin/demo/HelloWorld.kt
+++ kotlin/src/com/kotlin/demo/HelloWorld.kt
@@ -1,5 +1,15 @@
 package com.kotlin.demo
 
+import java.util.function.Consumer
+
 fun main(args: Array<String>) {
     println("hello world")
+    var list: List<String> = listOf("hello","world","kotlin")
+    for (str in list){
+        println(str)
+    }
+
+    println("----------")
+    list.forEach(Consumer { println(it) })
+    list.forEach(System.out::println)
 }
