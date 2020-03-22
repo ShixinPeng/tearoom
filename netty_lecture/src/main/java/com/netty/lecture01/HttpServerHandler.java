04702b7a8e6646ffb24c9b513083de30558dba01
Peng_shixin <shixinpeng0429@163.com>
2020-03-22 15:26:37 +0800

    添加打印埋点顺序

--- netty_lecture/src/main/java/com/netty/lecture01/HttpServerHandler.java
+++ netty_lecture/src/main/java/com/netty/lecture01/HttpServerHandler.java
@@ -7,6 +7,8 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 /**
  * @author shixinpeng
  * @description http服务的handler
@@ -24,17 +26,66 @@ public class HttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception {
 
-        System.out.println("接收到请求 channelRead0 " + channelHandlerContext.name() + " " + httpObject.toString());
-
+        System.out.println("接收到请求 channelRead0 " + httpObject.toString());
+        // 判断类型
         if (httpObject instanceof HttpRequest) {
-            // 判断类型
-            ByteBuf content = Unpooled.copiedBuffer("Hello world", CharsetUtil.UTF_8);
-            DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
-            response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plan");
-            response.headers().set(HttpHeaderNames.CONTENT_LENGTH,content.readableBytes());
-            // 把响应写回客户端
-            channelHandlerContext.writeAndFlush(response);
-        }
+            HttpRequest httpRequest = (HttpRequest)httpObject;
+            System.out.println("请求方法名：" + httpRequest.method().name());
+            URI uri = new URI(httpRequest.uri());
+            System.out.println("请求path：" + uri.getPath());
+            if (uri.getPath().equals("/hello")){
 
+                ByteBuf content = Unpooled.copiedBuffer("Hello world", CharsetUtil.UTF_8);
+                DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
+                response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plan");
+                response.headers().set(HttpHeaderNames.CONTENT_LENGTH,content.readableBytes());
+                // 把响应写回客户端
+                channelHandlerContext.writeAndFlush(response);
+            }
+
+        }
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Channel handlerAdded");
+        super.handlerAdded(ctx);
+    }
+    /**
+     * 连接活动
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Channel channelActive");
+        super.channelActive(ctx);
+    }
+
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Channel channelInactive");
+        super.channelInactive(ctx);
+    }
+
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Channel channelRegistered");
+        super.channelRegistered(ctx);
+    }
+
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Channel channelUnregistered");
+        super.channelUnregistered(ctx);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Channel handlerRemoved");
+        super.handlerRemoved(ctx);
     }
 }
