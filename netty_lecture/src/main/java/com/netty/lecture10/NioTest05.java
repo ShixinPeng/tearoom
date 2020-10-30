7ecb1c72dd17b8c760a260eaaf445b4b5a997cc0
Peng_shixin <shixinpeng0429@163.com>
2020-10-30 22:15:01 +0800

    使用堆外内存

--- netty_lecture/src/main/java/com/netty/lecture10/NioTest05.java
+++ netty_lecture/src/main/java/com/netty/lecture10/NioTest05.java
@@ -1,4 +1,52 @@
 package com.netty.lecture10;
 
+import java.lang.reflect.Field;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+
+/**
+ * 只读byteBuffer
+ * 堆外buffer
+ * scattering与Gathering
+ *
+ */
 public class NioTest05 {
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+
+        ByteBuffer direct = ByteBuffer.allocateDirect(10);
+
+        Field[] fields = Buffer.class.getFields();
+        for (int i = 0; i < fields.length; i++) {
+            System.out.println("field="+fields[i].getName());
+        }
+
+        Field[] declaredFields = Buffer.class.getDeclaredFields();
+
+        for (int i = 0; i < declaredFields.length; i++) {
+            System.out.println("declaredField="+declaredFields[i].getName());
+        }
+        Field field = Buffer.class.getDeclaredField("address");
+
+        field.setAccessible(true);
+
+        Object o = field.get(direct);
+
+        System.out.println("address="+o);
+
+    }
+
+    public static void readOnlyBuffer(){
+        // HeapByteBuffer
+        ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+
+        for (int i = 0; i < byteBuffer.capacity(); i++) {
+            byteBuffer.put((byte)i);
+        }
+
+        ByteBuffer readOnlyBuffer = byteBuffer.asReadOnlyBuffer();
+
+        System.out.println(readOnlyBuffer.getClass());
+        // java.nio.HeapByteBufferR
+    }
 }
